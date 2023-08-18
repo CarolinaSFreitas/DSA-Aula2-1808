98fd3dd0df508abb0dced0893783c6abc41db2b9
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-08-18 11:33:53 -0300

    Add files via upload

--- filmeController.js
+++ filmeController.js
@@ -1,12 +1,28 @@
 import { Filme } from "../models/Filme.js"
 
-export async function filmeIndex(req, res){
+export async function filmeIndex(req, res) {
     try {
         const filmes = await Filme.findAll()
         res.status(200).json(filmes)
     } catch (error) {
         res.status(400).send(error)
     }
+}
 
+export async function filmeCreate(req, res) {
+    const { titulo, genero, duracao, preco, datalan } = req.body()
 
+    if (!titulo || !genero || !duracao || !preco || !datalan) {
+        res.status(400).json("Erro... Informe todos os atributos.")
+        return
+    }
+
+    try {
+        const filme = await Filme.create({
+            titulo, genero, duracao, preco, datalan
+        })
+        res.status(201).json(filme)
+    } catch (error) {
+        res.status(400).send(error)
+    }
 }
