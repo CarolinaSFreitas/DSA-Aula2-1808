95fe3b4df85e492d2a38f1e2e9d30823d4db61a9
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-08-18 10:28:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { sequelize } from './database/conecta.js'
+import { Filme } from './models/Filme.js'
+
 const app = express()
 const port = 3000
 
@@ -11,12 +13,13 @@ async function conecta_db() {
     try {
         await sequelize.authenticate();
         console.log('Conexão bem sucedida.');
+        await Filme.sync()              //vai ciar a tabela no banco(se nao existir já)
+        console.log("Tabela de Filmes: Ok!")
     } catch (error) {
         console.error('Impossível conectar ao banco de dados:', error);
     }
 }
 conecta_db()
-
 
 app.listen(port, () => {
     console.log(`API de Filmes Rodando na Porta ${port}`)
